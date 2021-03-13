a2b977dbc245f292a30f592432a1ce7f68865249
Ari Oinas <ari@gbuilder.com>
2021-03-13 20:01:05 +0200

    Cleaned up some code. Hopefully fixed the delay bug

--- src/main.cpp
+++ src/main.cpp
@@ -20,11 +20,9 @@ enum MODE
     SYSTEM = HIGH // Switch OFF (-)
 };
 
-
 int backButtonState = 0;
 uint8_t joystickButtonIndex_Sim_Back = 0;
 uint8_t joystickButtonIndex_System_Back = 1;
-
 
 CustomRotaryEncoder encoders[5] = {
     CustomRotaryEncoder(2, 3, 4, 2), // Every encoder produces 6 joystick buttons
@@ -60,12 +58,9 @@ MODE mode = NOT_DEFINED;
 unsigned long previousStatusTime = 0;
 int statusInterval = 30 * 1000; // Every 10 seconds
 
-
 // the setup function runs once when you press reset or power the board
 void setup()
 {
-    // initialize digital pin LED_BUILTIN as an output.
-    //pinMode(LED_BUILTIN, OUTPUT);
     pinMode(PIN_MODE_SWITCH, INPUT);
     pinMode(PIN_BACK_BUTTON, INPUT);
 
@@ -74,53 +69,40 @@ void setup()
     pinMode(21, INPUT); // Encoder clicks // TODO: move to CustomRotaryEncoder
     pinMode(14, INPUT); // Encoder clicks // TODO: move to CustomRotaryEncoder
     pinMode(10, INPUT); // Encoder clicks // TODO: move to CustomRotaryEncoder
-    
 
     Serial.begin(57600);
     Joystick.begin(true);
     
-
-    
 }
-// the loop function runs over and over again forever
-void loop()
+
+void printStatus()
 {
+    Serial.print(millis() / 1000); // Runtime in seconds
+    Serial.print(" STATUS: ");
 
-    setMode(digitalRead(PIN_MODE_SWITCH));
-    
-    setEncoderDirections();
-    
-    // Handle back button
-    int currBackButtonState = digitalRead(PIN_BACK_BUTTON);
+    Serial.print("   Mode: ");
+    Serial.print(mode == SYSTEM ? "SYSTEM" : "CAR");
 
-    if ( currBackButtonState != backButtonState)
+    Serial.print(" Encoder count: ");
+    Serial.print(getEncoderCount());
+
+    Serial.println();
+
+    digitalWrite(LED_BUILTIN, mode == SYSTEM ? HIGH : LOW);
+}
+
+void setMode(int newMode)
+{
+    if (mode != newMode)
     {
-        backButtonState = currBackButtonState;
+        mode = newMode == SIM ? SIM : SYSTEM;
 
-        uint8_t joyBtn = mode == MODE::SIM ? joystickButtonIndex_Sim_Back : joystickButtonIndex_System_Back;
-        if ( backButtonState == HIGH )
-            Joystick.pressButton(joyBtn);
-        else
-            Joystick.releaseButton(joyBtn);
-    }
+        // Release buttons
+        Joystick.releaseButton(joystickButtonIndex_Sim_Back);
+        Joystick.releaseButton(joystickButtonIndex_System_Back);
 
-/*
-    if (millis() - previousStatusTime > statusInterval)
-    {
         printStatus();
-        previousStatusTime = millis();
     }
-*/
-    /*
-   // Blinking led
-  int delayInMs = mode ? 200 : 1000;
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(delayInMs);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(delayInMs);                       // wait for a second
-  */
-
-    //delay(10); // Just some delay so we dont run too fast :)
 }
 
 void setEncoderDirections()
@@ -136,7 +118,7 @@ void setEncoderDirections()
             ENCODER_ACTION action = direction == RotaryEncoder::Direction::CLOCKWISE ? ENCODER_ACTION::INCREASE : ENCODER_ACTION::DECREASE;
             uint8_t button = encoders[i].getButtonIndex(mode == SIM, action);
             Joystick.pressButton(button);
-            delay(100); // Debug
+            delay(10);
             Joystick.releaseButton(button);
 
             Serial.print("Encoder action: ");
@@ -158,64 +140,34 @@ void setEncoderDirections()
     }
 }
 
-void setMode(int newMode)
+// the loop function runs over and over again forever
+void loop()
 {
-    if (mode != newMode)
+
+    setMode(digitalRead(PIN_MODE_SWITCH));
+    
+    setEncoderDirections();
+    
+    // Handle back button
+    int currBackButtonState = digitalRead(PIN_BACK_BUTTON);
+
+    if ( currBackButtonState != backButtonState)
     {
-        mode = newMode == SIM ? SIM : SYSTEM;
+        backButtonState = currBackButtonState;
 
-        // Release buttons
-        Joystick.releaseButton(joystickButtonIndex_Sim_Back);
-        Joystick.releaseButton(joystickButtonIndex_System_Back);
-
-        printStatus();
+        uint8_t joyBtn = mode == MODE::SIM ? joystickButtonIndex_Sim_Back : joystickButtonIndex_System_Back;
+        if ( backButtonState == HIGH )
+            Joystick.pressButton(joyBtn);
+        else
+            Joystick.releaseButton(joyBtn);
     }
 }
 
-void printStatus()
-{
-    Serial.print(millis() / 1000); // Runtime in seconds
-    Serial.print(" STATUS: ");
 
-    Serial.print("   Mode: ");
-    Serial.print(mode == SYSTEM ? "SYSTEM" : "CAR");
 
-    Serial.print(" Encoder count: ");
-    Serial.print(getEncoderCount());
 
-    Serial.println();
 
-    digitalWrite(LED_BUILTIN, mode == SYSTEM ? HIGH : LOW);
-}
 
-void executeFunction(int encoderIndex, RotaryEncoder::Direction direction)
-{
-    /*
-    unsigned int keyIndex = direction == RotaryEncoder::Direction::COUNTERCLOCKWISE ? 0 : 1;
-    keyIndex += mode == SIM ? 0 : 3;
-    char keyboardCmd = encoders[encoderIndex].getKey(keyIndex);
 
-    Serial.print("Encoder #");
-    Serial.print(encoderIndex+1);
-    Serial.print(": Key command: ");
-    Serial.println( keyboardCmd );
-*/
-    //Keyboard.press(keyboardCmd);
-    //Keyboard.releaseAll();
-}
 
-void executeClickFunction(int encoderIndex)
-{
-    /*
-    unsigned int keyIndex = 2;
-    keyIndex += mode == SIM ? 0 : 3;
-    char keyboardCmd = encoders[encoderIndex].getKey(keyIndex);
 
-    Serial.print("Encoder #");
-    Serial.print(encoderIndex+1);
-    Serial.print(" clicked: ");
-    Serial.print(" Key command: ");
-    Serial.println( keyboardCmd );
-*/
-
-}
